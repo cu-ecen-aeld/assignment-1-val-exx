915c0065a62ef61ff992976eabd8978ceca1e3e1
val-exx <104652276+val-exx@users.noreply.github.com>
2026-04-01 14:54:22 +0200

    Update username validation test to check equality

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,7 +18,5 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    char *username_from_conf = malloc_username_from_conf_file();
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
-    free(username_from_conf);
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(),malloc_username_from_conf_file(),"Username match!");
 }
